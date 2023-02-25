2c7471c51d60ce2039207342109318dc360d2e23
Zé Antonio Calcopietro Jr <jr.calcopietro@gmail.com>
2023-02-25 16:06:05 -0300

    feat: add CRUD operations

--- src/index.js
+++ src/index.js
@@ -16,6 +16,51 @@ app.get("/", (_, res) => {
   res.send("try GET /contatos");
 });
 
+app.get("/contatos", (_, res) => {
+  const contatos = selectAll();
+  return res.status(200).send(contatos);
+});
+
+app.get("/contatos/:id", (req, res) => {
+  const { id } = req.params;
+
+  const contato = selectById(id);
+  if (contato) {
+    return res.status(200).send(contato);
+  }
+
+  return res.status(404).end();
+});
+
+app.post("/contatos", (req, res) => {
+  const { body: contato } = req;
+  const id = insert(contato);
+  res.status(200).send({ id });
+});
+
+app.put("/contatos/:id", (req, res) => {
+  const { id } = req.params;
+  const { body: contato } = req;
+
+  const rowsAffected = update(id, contato);
+  if (rowsAffected) {
+    return res.status(200).send({ rowsAffected });
+  }
+
+  return res.status(404).end();
+});
+
+app.delete("/contatos/:id", (req, res) => {
+  const { id } = req.params;
+
+  const rowsAffected = _delete(id);
+  if (rowsAffected) {
+    return res.status(200).send({ rowsAffected });
+  }
+
+  return res.status(404).end();
+});
+
 app.listen(port, () => {
   console.log(`agenda-api is listening on port ${port}`);
 });
